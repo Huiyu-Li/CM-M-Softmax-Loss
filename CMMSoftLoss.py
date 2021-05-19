72810ccd6f5cefce1fce626cda23ce0b10d094a3
Cerry <lihuiyu20@163.com>
2021-05-19 17:03:46 +0800

    reinitialized files

---
+++ CMMSoftLoss.py
@@ -0,0 +1,56 @@
+from __future__ import print_function
+from __future__ import division
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import Parameter
+import math
+###liuweihua @2021 @BJ @BIT
+###CMMSOftmax-for quality-imbalance learning
+class CMMSoftLoss(nn.Module):
+    r"""Implement of large margin cosine distance: :
+    Args:
+        in_features: size of each input sample
+        out_features: size of each output sample
+        s: norm of input feature
+        m: margin
+        cos(theta) - m
+    """
+
+    def __init__(self, in_features, out_features, s=30.0, m=0.40):
+        super(CMMSoftLoss, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.s = s
+        self.m = m
+        self.weight = Parameter(torch.FloatTensor(out_features, in_features))
+        nn.init.xavier_uniform_(self.weight)
+
+    def forward(self, input, label):
+        #----------------------------norm(x)---------------
+        norm_x= torch.norm(input,2,1)
+        # --------------------------- cos(theta) & phi(theta) ---------------------------
+        cosine = F.linear(F.normalize(input), F.normalize(self.weight))
+        phi = cosine - self.m
+        # --------------------------- convert label to one-hot ---------------------------
+        one_hot = torch.zeros(cosine.size(), device='cuda')
+        # one_hot = one_hot.cuda() if cosine.is_cuda else one_hot
+        one_hot.scatter_(1, label.view(-1, 1).long(), 1)
+        # -------------torch.where(out_i = {x_i if condition_i else y_i) -------------
+        output = (one_hot * phi) + ((1.0 - one_hot) * cosine)  # you can use torch.where if your torch.__version__ is 0.4
+        output *= self.s
+        # print(output)
+
+        #################################################squa map by norm
+        sig_norm=torch.sigmoid(norm_x)
+        sig_norm_dlg = torch.diag(sig_norm)
+        output = torch.mm(sig_norm_dlg,output)
+
+        return output
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(' \
+               + 'in_features=' + str(self.in_features) \
+               + ', out_features=' + str(self.out_features) \
+               + ', s=' + str(self.s) \
+               + ', m=' + str(self.m) + ')'
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Contraction-Mapping-of-Feature-Norms-for-Data-Quality-Imbalance-Learning
-Code for papper "Contraction Mapping of Feature Norms for Data Quality Imbalance Learning"
+
+# 0. Introduction
+This repository contains Pytorch code for the paper entitled with"Contraction Mapping of Feature Norms for Data Quality Imbalance Learning" . This paper will be released soon.
+
+In the field of  deep learning-based image classification,  the data for training image classifiers usually have different quality. Hence, the  low-quality data are hard to correctly classify.  In this paper, we discover that there is a positive correlation between the quality of a sample and its feature norm learned with softmax loss. Therefor, we propose a contraction mapping function of feature norms and use it to develop novel softmax losses.  Experiments on various classification applications, including handwritten digit recognition, lung nodule classification, face verification, and face recognition, demonstrate that the proposed approach is promising to effectively deal with the problem of learning data with different quality and leads to significant and stable improvements in the classification accuracy.
+
+# 1. Usage
+#### CMMSoftLoss.py
+The proposed contraction mapping of feature norms, which is embedded into classical and large margin-based softmax losses to produce new learning objectives.
+
+#### MNIST_with_CMMLoss.py
+The evaluation of  the proposed CM-M-Softmax losses in handwritten digit classification.
+
+Visualization of 2-D features of the test samples in MNIST, learned by using (a) Softmax; (b) L2-softmax; (c) CM-M-Softmax; (d) Margin&L2-Softmaxloss; (e) Margin&CM-M-Softmax.
+
+| (a) Softmax           | L2-softmax            | CM-M-Softmax          | Margin&L2-Softmaxloss | Margin&CM-M-Softmax   |
+| --------------------- | --------------------- | --------------------- | --------------------- | --------------------- |
+| ![1](.\figures\1.jpg) | ![2](.\figures\2.jpg) | ![3](.\figures\3.jpg) | ![4](.\figures\4.jpg) | ![5](.\figures\5.jpg) |
+
